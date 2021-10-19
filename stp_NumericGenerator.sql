99274f5e3d56a6a35783d6a9957ee5a231ed2cc0
Maksym Bondaruk <b.max.lw@gmail.com>
2021-10-19 17:57:53 +0300

    [+] NumericGenerator added

---
+++ stp_NumericGenerator.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE stp_Salary_INS @id INT, @max_id INT AS
+BEGIN TRY
+	BEGIN
+		WHILE (@id != @max_id)
+		BEGIN
+			UPDATE Employees SET Salary = ROUND(RAND() * (35000 - 10000) + 10000, 2) WHERE EmpID = @id;
+			SET @id = @id + 1;
+		END
+	END 
+END TRY
+BEGIN CATCH
+	BEGIN
+		PRINT N'FATAL ERROR!'
+	END
+END CATCH;
+
+EXEC stp_Salary_INS 1, 1001;
+DROP PROCEDURE stp_Salary_INS;
